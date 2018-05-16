b939bc749a8cfdb7dee5d03dafd0477f1f5de243
koo <zhengqun.koo@gmail.com>
2018-05-16 15:09:31 +0800

    Restructured django files in taxibros.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from taxibros import __version__
 
-setup(name='taxibros',
-      version='0.0.1',
-      description='Instantly query taxi arrival times',
-      url='http://github.com/zhengqunkoo/taxibros',
-      author='zhengqunkoo',
-      author_email='zhengqun.koo@gmail.com',
-      packages=['taxibros'],
-      install_requires=[
-          'bson',
-      ],
-      zip_safe=False)
+setup(
+    name='taxibros',
+    version=__version__,
+    description='Instantly query taxi arrival times',
+
+    url='http://github.com/zhengqunkoo/taxibros',
+    author='zhengqunkoo',
+    author_email='zhengqun.koo@gmail.com',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'bson',
+        'django',
+        'django-dotenv',
+    ],
+    zip_safe=False
+)
---
+++ taxibros/settings/development.py
@@ -0,0 +1,9 @@
+from taxibros.settings.production import *
+
+DEBUG = True
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
+
+INTERNAL_IPS = ('127.0.0.1',)
