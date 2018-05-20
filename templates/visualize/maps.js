158c0dcdc94c6c791aed426bb1a37fee035ce7b6
seannpn <seanngpengnam@u.nus.edu>
2018-05-20 14:23:42 +0800

    allow for dynamic visualisation of points using django templating

---
+++ templates/visualize/maps.js
@@ -0,0 +1,57 @@
+//Javacript file is "dynamically" generated using django's template generation
+var map, heatmap;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11.6,
+    center: {lat: 1.3521, lng: 103.8198},
+    mapTypeId: 'satellite'
+  });
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: getPoints(),
+    map: map
+  });
+}
+
+function toggleHeatmap() {
+  heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+  var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ]
+  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function changeRadius() {
+  heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
+
+function changeOpacity() {
+  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
+
+function getPoints() {
+  return [
+    {% for coord in coordinates %}
+      new google.maps.LatLng({{ coord.lat }}, {{ coord.long }}),
+    {% endfor %}
+    new google.maps.LatLng(37.752986, -122.403112),
+    new google.maps.LatLng(37.751266, -122.403355)
+  ];
+}
--- visualize/views.py
+++ visualize/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-
+from daemons.models import Coordinate
 def index(request):
     """View function for home page of site."""
 
-    # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
-        'visualize/index.html',
+        'visualize/index.html'
     )
+def genjs(request):
+    # Render the js template index.html with the data in the context variable.
+    coordinates = Coordinate.objects.all()
+    context = {'coordinates':coordinates}
+
+    return render(
+            request,
+            'visualize/maps.js',
+            context
+            )
