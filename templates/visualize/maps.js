c7e341df8c3cd55ff4bbcc2c12d5b657f6341fda
koo <zhengqun.koo@gmail.com>
2018-05-23 11:47:31 +0800

    Add geolocation in browser.

--- templates/visualize/maps.js
+++ templates/visualize/maps.js
@@ -1,5 +1,10 @@
 // Javacript file is "dynamically" generated using django's template generation
-var map, heatmap;
+//
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+var map, heatmap, infoWindow;
 var pointArray;
 
 function initMap() {
@@ -15,6 +20,28 @@ function initMap() {
     data: pointArray,
     map: map
   });
+
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
 function toggleHeatmap() {
@@ -79,4 +106,12 @@ function minutesChange(e) {
           alert("Failed to reach {% url 'visualize:gen.js' %}.");
       }
   });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
