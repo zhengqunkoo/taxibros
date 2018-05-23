942b758a54fb9abe4f5093af0678406cb5c8146f
pengnam <seanngpengnam@gmail.com>
2018-05-23 20:47:56 +0800

    edit html for checks

--- visualize/views.py
+++ visualize/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from daemons.models import Coordinate, Timestamp
+from background_task.models import Task
 from django.utils import timezone
 from itertools import chain
 import datetime
@@ -7,6 +8,22 @@ import os
 
 def index(request):
     """View function for home page of site."""
+    #CHECK1:If daemon is running
+    if (Task.objects.all().count()==0):
+        return render(
+            request,
+            'visualize/index.html',
+            {"api_key":os.getenv("GOOGLEMAPS_SECRET_KEY"), "error_message": "Im sorry. The service appears to be experiencing a malfunction."}
+        )
+    #CHECK2:If there is insufficient data
+    times = Timestamp.objects.filter(date_and_time__range = [timezone.now() - datetime.timedelta(minutes=5), timezone.now()])
+    if (times.count()<5):
+        return render(
+            request,
+            'visualize/index.html',
+            {"api_key":os.getenv("GOOGLEMAPS_SECRET_KEY"), "error_message": "Data is still incomplete, please wait a few minutes before refreshing."}
+        )
+
     return render(
         request,
         'visualize/index.html',
