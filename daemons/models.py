5fdc424113c4a363d7eaf08d17d5ee23016f4ae6
koo <zhengqun.koo@gmail.com>
2018-05-24 20:31:37 +0800

    Remove unnecessary string formatting.

--- daemons/models.py
+++ daemons/models.py
@@ -7,12 +7,6 @@ class Timestamp(models.Model):
     """
     date_time = models.DateTimeField()
     
-    def __str__(self):
-        """
-        String for representing the Model object.
-        """
-        return str(self.date_time)
-    
 
 class Coordinate(models.Model):
     """Model representing a coordinate recorded at timestamp.
