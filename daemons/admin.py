47b48591f5538f9bc5521d1281b7301e3ffc0e58
koo <zhengqun.koo@gmail.com>
2018-05-24 20:39:12 +0800

    Skip confirmation page for selected objects.
    Skips when deleting an object by selecting checkbox(es), selecting "Delete selected" action, and pressing "Go".
    Does not skip confirmation page when deleting individual objects.

--- daemons/admin.py
+++ daemons/admin.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 from .models import Timestamp, Coordinate
 
 
+def delete_selected(modeladmin, request, queryset):
+    queryset.delete()
+
+
 @admin.register(Timestamp)
 class TimestampAdmin(admin.ModelAdmin):
     list_display = ('date_time',)
+    actions = (delete_selected,)
 
 
 @admin.register(Coordinate)
