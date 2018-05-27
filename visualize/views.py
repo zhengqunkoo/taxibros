zhengqunkoo/taxibros

commit 077fe9cdb397bcc5d0ec9456131c76f9f9609882

Simplified error notifications.

visualize/views.py
@@ -9,29 +9,24 @@ from django.http import JsonResponse
 
 def index(request):
     """View function for home page of site."""
+    context = {
+        "GOOGLEMAPS_SECRET_KEY": settings.GOOGLEMAPS_SECRET_KEY,
+        "SLIDE_EVENT": settings.SLIDE_EVENT,
+    }
+
     #CHECK1:If daemon is running
     if (Task.objects.all().count()==0):
-        return render(
-            request,
-            'visualize/index.html',
-            {"api_key":os.getenv("GOOGLEMAPS_SECRET_KEY"), "error_message": "Im sorry. The service appears to be experiencing a malfunction."}
-        )
+        context["error_message"] = "Im sorry. The service appears to be experiencing a malfunction."
+
     #CHECK2:If there is insufficient data
-    times = Timestamp.objects.filter(date_and_time__range = [timezone.now() - datetime.timedelta(minutes=5), timezone.now()])
+    times = Timestamp.objects.filter(date_time__range = [timezone.now() - datetime.timedelta(minutes=5), timezone.now()])
     if (times.count()<5):
-        return render(
-            request,
-            'visualize/index.html',
-            {"api_key":os.getenv("GOOGLEMAPS_SECRET_KEY"), "error_message": "Data is still incomplete, please wait a few minutes before refreshing."}
-        )
+        context["error_message"] = "Data is still incomplete, please wait a few minutes before refreshing."
 
     return render(
         request,
         'visualize/index.html',
-        {
-            "GOOGLEMAPS_SECRET_KEY": settings.GOOGLEMAPS_SECRET_KEY,
-            "SLIDE_EVENT": settings.SLIDE_EVENT,
-        }
+        context
     )
 
 def get_coordinates(request):

commit 283086129d609168b7d50f9bea5f29a7ca257d10

Error message was overriden by CHECK2. Render immediately on error.
Was broken by commit 077fe9cdb397bcc5d0ec9456131c76f9f9609882.

visualize/views.py
@@ -20,12 +20,23 @@ def index(request):
     #CHECK1:If daemon is running
     if (Task.objects.all().count()==0):
         context["error_message"] = "Im sorry. The service appears to be experiencing a malfunction."
+        return render(
+            request,
+            'visualize/index.html',
+            context
+        )
 
     #CHECK2:If there is insufficient data
     times = Timestamp.objects.filter(date_time__range = [timezone.now() - datetime.timedelta(minutes=5), timezone.now()])
     if (times.count()<5):
         context["error_message"] = "Data is still incomplete, please wait a few minutes before refreshing."
+        return render(
+            request,
+            'visualize/index.html',
+            context
+        )
 
+    # Else: render with normal context.
     return render(
         request,
         'visualize/index.html',