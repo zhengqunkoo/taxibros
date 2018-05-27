8f5f6c053de573dd01146160d5ff10ce1ccacd4e
koo <zhengqun.koo@gmail.com>
2018-05-27 19:53:57 +0800

    Remove initial geolocation. Geolocate only on clicking 'show nearby'.

--- templates/visualize/maps.js
+++ templates/visualize/maps.js
@@ -22,27 +22,6 @@ function initMap() {
   });
 
   infoWindow = new google.maps.InfoWindow;
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
 }
 
 function toggleHeatmap() {
@@ -147,6 +126,7 @@ function showNearby() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
 }
+
 function minutesChange(e) {
   // Asynchronously update maps with serialized coordinates.
   var minutes = e.value;
