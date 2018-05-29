14e52970d84ebaf56aae1e6bce7fcc13272c17f5
pengnam <seanngpengnam@gmail.com>
2018-05-29 22:17:24 +0800

    change to vertical bar chart w axes

--- templates/visualize/maps.js
+++ templates/visualize/maps.js
@@ -205,29 +205,44 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 
 function drawChart(day_stats) {
-    var width = 420,
-        barHeight = 20;
+    var height = 420,
+        barWidth = 10;
+    var margin = {top: 20, right: 10, bottom: 20, left: 40};
+    height = height - margin.top - margin.bottom;
 
-    var x = d3.scaleLinear()
+    var y = d3.scaleLinear()
         .domain([0, d3.max(day_stats)])
-        .range([0, 420]);
+        .range([0, height]);
+
+    var yAxis = d3.axisLeft(y)
+        .scale(y)
+        .ticks(10, "%");
     var chart = d3.select(".chart")
-        .attr("width", width);
-    chart.attr("height", barHeight * day_stats.length);
+        .attr("height", height);
+    chart.attr("width", barWidth * day_stats.length);
 
     var bar = chart.selectAll("g")
         .data(day_stats)
       .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
+        .attr("transform", function(d, i) { return "translate(" + (margin.left + (i * barWidth)) + "," + (height-y(d)) + ")"; });
     bar.append("rect")
-        .attr("width", function(d) { return x(d); })
-        .attr("height", barHeight - 1);
+        .attr("height", function(d) { return y(d); })
+        .attr("width", barWidth - 1)
 
     bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+        .attr("y", function(d) { return y(d) - 3; })
+        .attr("x", barWidth / 2)
+        .attr("dx", ".10em")
+        .text(function(d) { return d; });
+
+    bar.transition()
+        .delay(function(d, i) { return i * 100; })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); });
+
+    chart.append("g")
+        .attr("transform", "translate(" + margin.left+ ",0)")
+        .call(yAxis);
+
 
 }
