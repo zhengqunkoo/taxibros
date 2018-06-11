48b2be005e64f3e97d4a048a741f3905a79795c9
pengnam <seanngpengnam@gmail.com>
2018-06-11 10:30:01 +0800

    Add tests for process closest roads and unique locations

--- daemons/tests/test_download.py
+++ daemons/tests/test_download.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.test import TestCase, override_settings
 from django.utils import timezone
 from daemons.download import DownloadJson, TaxiAvailability
-from daemons.models import Timestamp
+from daemons.models import Timestamp, Location
 
 
 @patch("daemons.download.DownloadJson.get_json")
@@ -128,6 +128,67 @@ class DownloadDatabaseTest(TestCase):
                 }
             ],
         }
+        cls._json_results2 = {
+            "type": "FeatureCollection",
+            "crs": {
+                "type": "link",
+                "properties": {
+                    "href": "http://spatialreference.org/ref/epsg/4326/ogcwkt/",
+                    "type": "ogcwkt",
+                },
+            },
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "MultiPoint",
+                        "coordinates": [
+                            [103.99528, 1.3754],
+                            [104.00055, 1.3809],
+                            [104.00057, 1.38101],
+                            [104.00072, 1.3807],
+                            [104.00098, 1.38424],
+                        ],
+                    },
+                    "properties": {
+                        "timestamp": "2017-05-15T13:02:58+08:00", #To simulate a second run
+                        "taxi_count": 5,
+                        "api_info": {"status": "healthy"},
+                    },
+                }
+            ],
+        }
+
+        cls._nearest_road_results = {
+            "snappedPoints": [
+                {
+                    "location": {
+                        "latitude": 1.343211721179933,
+                        "longitude": 103.76092847271761,
+                    },
+                    "originalIndex": 1,
+                    "placeId": "ChIJqSxg_mgQ2jERVYEAKmGw0aw",
+                },
+                {
+                    "location": {
+                        "latitude": 1.3449623148300323,
+                        "longitude": 103.76088042484533,
+                    },
+                    "originalIndex": 2,
+                    "placeId": "ChIJNWdwM2gQ2jER0Gamoz7a4bU",
+                },
+                {
+                    "location": {
+                        "latitude": 1.3449623148300323,
+                        "longitude": 103.76088042484533,
+                    },
+                    "originalIndex": 2,
+                    "placeId": "ChIJNWdwM2gQ2jER0Wamoz7a4bU",
+                },
+            ]
+        }
+        cls._closest_road_result1 = ["0","1","2"]
+        cls._closest_road_result2 = ["2","3","4"]
 
     # Make start_date_time always one minute before end_date_time (now).
     @override_settings(
@@ -152,3 +213,29 @@ class DownloadDatabaseTest(TestCase):
         self.assertEquals(Timestamp.objects.all().count(), 1)
         self._ta.download()
         self.assertEquals(Timestamp.objects.all().count(), 1)
+
+    @patch("daemons.download.DownloadJson.get_json")
+    def test_process_closest_roads(self, mock_get_json):
+        """Check if closest roads are returned in the right format given a json response."""
+        mock_get_json.return_value = self._nearest_road_results
+        expected = [None, "ChIJqSxg_mgQ2jERVYEAKmGw0aw", "ChIJNWdwM2gQ2jER0Wamoz7a4bU"]
+        actual = self._ta.get_closest_roads([(0, 0), (1, 1), (2, 2)])
+        self.assertEquals(expected,actual)
+
+    @patch("daemons.download.DownloadJson.get_closest_roads")
+    @patch("daemons.download.DownloadJson.get_json")
+    def test_unique_location(self, mock_get_json, mock_get_closest_roads):
+        """Download two timestamps, each with locations that both share and are unique to each
+        timestamp. The right number should be returned after each call"""
+
+        self.assertEquals(Location.objects.all().count(), 0)
+        #First download
+        mock_get_json.return_value = self._json_results
+        mock_get_closest_roads.return_value = self._closest_road_result1
+        self._ta.download()
+        self.assertEquals(Location.objects.all().count(), 3)
+        #Second download
+        mock_get_json.return_value = self._json_results2
+        mock_get_closest_roads.return_value = self._closest_road_result2
+        self._ta.download()
+        self.assertEquals(Location.objects.all().count(), 5)
