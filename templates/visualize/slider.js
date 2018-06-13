7abe7a2ecdde4b54efd6f877ce3f70dc3006de8a
koo <zhengqun.koo@gmail.com>
2018-06-13 11:06:08 +0800

    Calendar resets slider. Partially address #32.
    Put slider in middle to scrub heatmaps before and after selected time.
    But if user picks time on calendar close to now, should not put in middle.
    Because then user can scrub to future (where there is no heatmap data).

--- templates/visualize/slider.js
+++ templates/visualize/slider.js
@@ -1,3 +1,4 @@
+// Update global date on all callbacks.
 var date = new Date();
 var MS_PER_MINUTE = 60000;
 
@@ -12,20 +13,62 @@ $('#genHeatmapIntensity').slider({
 
 $('#datetimepicker').datetimepicker({
   date: date
-})
+}).on('dp.hide', pickDate);
 
-function minutesToDate(minutes) {
-  return new Date(date - minutes * MS_PER_MINUTE);
+function pickDate(e) {
+  /**
+   * Reset sliders to middle.
+   * Set min and max value of sliders relative to picked date.
+   */
+
+  // Update global date on all callbacks.
+  date = new Date();
+
+  // Convert date to minutes (to be set for sliders).
+  var minutes = dateToMinutes(e.date);
+  var genTime = $('#genTime');
+  var genHeatmap = $('#genHeatmap');
+
+  var genTimeRange = genTime.data('sliderMax') - genTime.data('sliderMin');
+  genTime.slider({
+    min: minutes - genTimeRange/2,
+    max: minutes + genTimeRange/2,
+  });
+  genTime.slider('setValue', minutes);
+
+  var genHeatmapRange = genHeatmap.data('sliderMax') - genHeatmap.data('sliderMin');
+  genHeatmap.slider({
+    min: minutes - genHeatmapRange/2,
+    max: minutes + genHeatmapRange/2,
+  });
+  genHeatmap.slider('setValue', minutes);
 }
 
-function dateToMinutes(date) {
-  return (new Date() - date) / MS_PER_MINUTE;
+function minutesToDate(m) {
+  /**
+   * Convert @param m to date, relative to global date variable.
+   */
+  return new Date(date - m * MS_PER_MINUTE);
+}
+
+function dateToMinutes(d) {
+  /**
+   * Convert @param d to minutes, relative to global date variable.
+   */
+  return (date - d) / MS_PER_MINUTE;
 }
 
 function dateSlider(sliderName, sliderChangeCallback) {
+  /**
+   * Create new slider.
+   * Bind slider event to datetimepicker date value.
+   */
   $(sliderName).slider({
     formatter: minutesToDate
   }).on('{{ SLIDE_EVENT }}', function(e) {
+
+    // Update global date on all callbacks.
+    date = new Date();
     sliderChangeCallback(e);
     $('#datetimepicker').datetimepicker('date', minutesToDate(e.value.newValue));
   });
