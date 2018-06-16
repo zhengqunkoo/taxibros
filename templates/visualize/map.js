e4f5ba84d147049c83e7cd0584a4b8557d29e941
pengnam <seanngpengnam@gmail.com>
2018-06-17 01:00:22 +0800

    Add x-axis for chart

--- templates/visualize/map.js
+++ templates/visualize/map.js
@@ -334,8 +334,8 @@ function drawChart() {
            var response = JSON.parse(this.responseText);
            day_stats = response.day_stats;
            var height = 210,
-               barWidth = 10;
-           var margin = {top: 20, right: 10, bottom: 20, left: 40};
+               barWidth = 20;
+           var margin = {top: 20, right: 10, bottom: 30, left: 40};
            height = height - margin.top - margin.bottom;
 
            var y = d3.scaleLinear()
@@ -344,9 +344,18 @@ function drawChart() {
            var yAxis = d3.axisLeft(y)
                .scale(y)
                .ticks(10, "s");
+
+           var parseTime = d3.timeParse("%I:%M %p");
+           var startTime = parseTime("06:00 AM");
+           var endTime = parseTime("05:00 AM");
+
+           var x = d3.scaleTime().domain([startTime, endTime]).range([0,barWidth * day_stats.length]);
+           var xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%I:%M %p")).ticks(10);
+
+
            var chart = d3.select(".chart")
-               .attr("height", height);
-           chart.attr("width", barWidth * day_stats.length);
+               .attr("height", height + margin.top + margin.bottom);
+           chart.attr("width", barWidth * day_stats.length + margin.left + margin.right);
 
            var rect = chart.selectAll("g")
                .data(day_stats)
@@ -364,6 +373,14 @@ function drawChart() {
            chart.append("g")
                .attr("transform", "translate(" + margin.left+ ",0)")
                .call(yAxis);
+           chart.append("g")
+                .attr("transform", "translate(" + margin.left + "," + height + ")")
+              .call(xAxis)
+              .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", ".15em")
+                .attr("transform", "rotate(-65)");
         }
     };
     xhttp.open("GET", "{% url 'visualize:chart' %}", true);
