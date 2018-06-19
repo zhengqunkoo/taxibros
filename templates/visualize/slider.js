zhengqunkoo/taxibros

commit db8def6ee8efd80d584be2e836c4d51e0906fd56

Add heatmap callback in pickDate callback function. Fix #32.
Move slider value parser into slider creation function.
So that genSliderCallback can take an integer value, as given by pickDate.
To see pickDate call heatmap at selected time, set HEATMAP_NOW=False in local_settings.py.

templates/visualize/slider.js
@@ -19,6 +19,8 @@ function pickDate(e) {
   /**
    * Reset sliders to middle.
    * Set min and max value of sliders relative to picked date.
+   * Call genHeatmapChange.
+   *   Assume when users pick date, they want heatmaps from long ago.
    */
 
   // Update global date on all callbacks.
@@ -42,6 +44,8 @@ function pickDate(e) {
     max: minutes + genHeatmapRange/2,
   });
   genHeatmap.slider('setValue', minutes);
+
+  genHeatmapSliderChange(minutes);
 }
 
 function minutesToDate(m) {
@@ -54,8 +58,19 @@ function minutesToDate(m) {
 function dateToMinutes(d) {
   /**
    * Convert @param d to minutes, relative to global date variable.
+   * @return minutes: integer.
    */
-  return (date - d) / MS_PER_MINUTE;
+  return ((date - d) / MS_PER_MINUTE) | 0;
+}
+
+function genSliderValue(e) {
+
+  // Extract value from slider event.
+  var value = e.value;
+  if (value.hasOwnProperty('newValue')) {
+    value = value.newValue;
+  }
+  return value;
 }
 
 function dateSlider(sliderName, sliderChangeCallback) {
@@ -69,7 +84,7 @@ function dateSlider(sliderName, sliderChangeCallback) {
 
     // Update global date on all callbacks.
     date = new Date();
-    sliderChangeCallback(e);
+    sliderChangeCallback(genSliderValue(e));
     $('#datetimepicker').datetimepicker('date', minutesToDate(e.value.newValue));
   });
 }

templates/visualize/map.js
@@ -173,20 +173,9 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 }
 
-function genSliderValue(e) {
-
-  // Extract value from slider event.
-  var value = e.value;
-  if (value.hasOwnProperty('newValue')) {
-    value = value.newValue;
-  }
-  return value;
-}
-
-function genSliderCallback(e, url, successCallback) {
+function genSliderCallback(value, url, successCallback) {
 
   // Asynchronously update maps.
-  var value = genSliderValue(e);
   $.ajax({
     url: url,
     data: {
@@ -220,11 +209,10 @@ function genHeatmapSliderChange(e) {
   });
 }
 
-function genHeatmapIntensitySliderChange(e) {
+function genHeatmapIntensitySliderChange(value) {
 
   // Filters on intensityArray. Assume intensityArray defined.
   // Only change pointArray if intensityArray has elements.
-  var value = genSliderValue(e);
   if (intensityArray.length != 0) {
     var intensities = intensityArray.filter(d => d[0] >= value);
     pointArray.clear();

commit 1a354a34a8beaf3c8b01831039f32505f7e9d9bf

Fix bug introduced by commit db8def6e.
Need parse value before passing to SliderChange function.

templates/visualize/slider.js
@@ -9,7 +9,9 @@ $('#genHeatmapIntensity').slider({
   formatter: function(value) {
     return value;
   }
-}).on('{{ SLIDE_EVENT }}', genHeatmapIntensitySliderChange);
+}).on('{{ SLIDE_EVENT }}', function(e) {
+  genHeatmapIntensitySliderChange(genSliderValue(e))
+});
 
 $('#datetimepicker').datetimepicker({
   date: date
@@ -64,8 +66,9 @@ function dateToMinutes(d) {
 }
 
 function genSliderValue(e) {
-
-  // Extract value from slider event.
+  /**
+   * Extract value from slider event.
+   */
   var value = e.value;
   if (value.hasOwnProperty('newValue')) {
     value = value.newValue;