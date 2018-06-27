c8829913d61c9496af00f2c8ada4225776ff0933
pengnam <seanngpengnam@gmail.com>
2018-06-27 13:30:12 +0800

    Modify itinerary table arrow button

--- visualize/static/js/itinerary.js
+++ visualize/static/js/itinerary.js
@@ -3,11 +3,10 @@ $(document).ready(function() {
       var rightVal = $("#container-itinerary").css("right");
       if (rightVal == '10px'){
           $('#container-itinerary').stop().animate({right:"-50%"}, 500);
-          $('#slider').prop("value","<<");
-
+          document.querySelector('.arrow').style.transform = ("rotate(-135deg)");
       } else {
           $('#container-itinerary').stop().animate({right:"10px"},500);
-          $('#slider').prop("value",">>");
+          document.querySelector('.arrow').style.transform = ("rotate(45deg)");
       }
   });
 });
