a71f35b3f20656c1f380e570dfba4d34ebb7790a
pengnam <seanngpengnam@gmail.com>
2018-07-15 17:59:23 +0800

    Add mobile context for map.js

--- visualize/views.py
+++ visualize/views.py
@@ -95,7 +95,7 @@ if settings.VISUALIZE_LOCATIONS:
             {
                 "coordinates": Location.objects.exclude(
                     lat=1.352083, lng=103.819836
-                ).all()
+                ).all(),
             },
         )
 
@@ -105,7 +105,8 @@ else:
     def map_js(request):
         """Render Javascript file with list of coordinates in context."""
         return render(
-            request, "visualize/map.js", {"coordinates": get_coordinates_time(request)}
+            request, "visualize/map.js", {"coordinates": get_coordinates_time(request),"mobile": request.user_agent.is_mobile}
+
         )
 
 
