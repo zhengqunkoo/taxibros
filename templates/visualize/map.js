59b6b2c86e964d3934fa5901f4090c9144c3779b
koo <zhengqun.koo@gmail.com>
2018-07-26 23:32:24 +0800

    Mouse does not pause on playback, retires visibility code.
    Test pausing, switching tabs, focusing on URL bar, on a laptop with and without mobile useragent.
    WIP: test if mouse does not pause on playback in an actual mobile phone.

--- templates/visualize/map.js
+++ templates/visualize/map.js
@@ -548,49 +548,6 @@ function unsetMapObj(obj) {
 }
 
 var mus = new Mus();
-var hidden, visibilityChange; 
-if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
-  hidden = "hidden";
-  visibilityChange = "visibilitychange";
-} else if (typeof document.msHidden !== "undefined") {
-  hidden = "msHidden";
-  visibilityChange = "msvisibilitychange";
-} else if (typeof document.webkitHidden !== "undefined") {
-  hidden = "webkitHidden";
-  visibilityChange = "webkitvisibilitychange";
-}
- 
-var videoElement = document.getElementById("videoElement");
-
-// Warn if the browser doesn't support addEventListener or the Page Visibility API
-if (typeof document.addEventListener === "undefined" || hidden === undefined) {
-  console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
-} else {
-
-  // If the page is hidden, pause the video;
-  // if the page is shown, play the video
-  function handleVisibilityChange() {
-    if (document[hidden]) {
-      videoElement.pause();
-    } else {
-      videoElement.play();
-    }
-  }
-
-  // Handle page visibility change   
-  document.addEventListener(visibilityChange, handleVisibilityChange, false);
-    
-  // When the video pauses, set the title.
-  // This shows the paused
-  videoElement.addEventListener("pause", function(){
-    document.title = 'Paused';
-  }, false);
-    
-  // When the video plays, set the title.
-  videoElement.addEventListener("play", function(){
-    document.title = 'Playing'; 
-  }, false);
-}
 
 window.onload = function() {
   mus.release();
